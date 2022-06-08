cb2088d47c21acc0f4e5e2d4a7e65d2281f8741b
ranazarafshani@gmail.com <ranazarafshani@gmail.com>
2022-06-08 18:56:47 +0430

    add Navbar

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ import {
     Financial, ColorPicker, ColorMapping, Editor
 } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider';
 import './App.css'
 
 const App = () => {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
 
     return (
         <div>
