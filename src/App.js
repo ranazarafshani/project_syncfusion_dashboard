c1ea6804f7fdda34a89960def56436e1672c16a2
ranazarafshani@gmail.com <ranazarafshani@gmail.com>
2022-06-09 14:14:53 +0430

    add Earning Section to HomePage

--- src/App.js
+++ src/App.js
@@ -40,37 +40,37 @@ const App = () => {
                     )}
                     <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
                         <Navbar />
+
+                        <div>
+                            <Routes>
+                                {/*Dashboard*/}
+                                <Route path="/" element={<Ecommerce />} />
+                                <Route path="/ecommerce" element={<Ecommerce />} />
+
+                                {/*Pages*/}
+                                <Route path="/orders" element={<Orders />} />
+                                <Route path="/employees" element={<Employees />} />
+                                <Route path="/customers" element={<Customers />} />
+
+                                {/*Apps*/}
+                                <Route path="/kanban" element={<Kanban />} />
+                                <Route path="/editor" element={<Editor />} />
+                                <Route path="/calendar" element={<Calendar />} />
+                                <Route path="/color-picker" element={<ColorPicker />} />
+
+                                {/*Charts*/}
+                                <Route path="/line" element={<Line />} />
+                                <Route path="/area" element={<Area />} />
+                                <Route path="/bar" element={<Bar />} />
+                                <Route path="/pie" element={<Pie />} />
+                                <Route path="/financial" element={<Financial />} />
+                                <Route path="/color-mapping" element={<ColorMapping />} />
+                                <Route path="/pyramid" element={<Pyramid />} />
+                                <Route path="/stacked" element={<Stacked />} />
+
+                            </Routes>
+                        </div>
                     </div>
-                    <div>
-                        <Routes>
-                            {/*Dashboard*/}
-                            <Route path="/" element={<Ecommerce />} />
-                            <Route path="/ecommerce" element={<Ecommerce />} />
-
-                            {/*Pages*/}
-                            <Route path="/orders" element={<Orders />} />
-                            <Route path="/employees" element={<Employees />} />
-                            <Route path="/customers" element={<Customers />} />
-
-                            {/*Apps*/}
-                            <Route path="/kanban" element={<Kanban />} />
-                            <Route path="/editor" element={<Editor />} />
-                            <Route path="/calendar" element={<Calendar />} />
-                            <Route path="/color-picker" element={<ColorPicker />} />
-
-                            {/*Charts*/}
-                            <Route path="/line" element={<Line />} />
-                            <Route path="/area" element={<Area />} />
-                            <Route path="/bar" element={<Bar />} />
-                            <Route path="/pie" element={<Pie />} />
-                            <Route path="/financial" element={<Financial />} />
-                            <Route path="/color-mapping" element={<ColorMapping />} />
-                            <Route path="/pyramid" element={<Pyramid />} />
-                            <Route path="/stacked" element={<Stacked />} />
-
-                        </Routes>
-                    </div>
-
                 </div>
             </BrowserRouter>
 
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
+import {
+    earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData,
+    SparklineAreaData, ecomPieChartData
+} from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
     return (
-        <div>Ecommerce</div>
+        <div className="mt-12">
+            <div className="flex flex-wrap lg:flex-nowrap justify-center">
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 
+                bg-hero-pattern bg-no-repeat bg-cover bg-center">
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <p className="font-bold text-gray-400">Earnings</p>
+                            <p className="text-2xl">$63,448.78</p>
+                        </div>
+                    </div>
+                    <div className="mt-6">
+                        <Button
+                            color="white"
+                            bgColor="blue"
+                            text="Download"
+                            borderRadius="10px"
+                            size="md"
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-wrap justify-center items-center gap-1 m-3">
+                    {earningData.map((item) => (
+                        <div key={item.title}
+                            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+                        >
+                            <button type="button"
+                                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                                className="text-2xl opacity-90 rounded-full p-4 hover:drop-shadow-xl">
+                                {item.icon}
+                            </button>
+                            <p className="mt-3">
+                                <span className="text-lg font-semibold">{item.amount}</span>
+                                <span className={`text-sm ml-2 text-${item.pcColor}`}>{item.percentage}</span>
+                            </p>
+                            <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
